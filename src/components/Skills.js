ecf570860c4e833a642be05347c277dfa24e4671
NosoaB17 <ngsn176@gmail.com>
2023-04-12 02:10:09 +0700

    Uploading

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -59,7 +59,7 @@ export const Skills = () => {
                 </div>
                 <div className="item">
                   <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
+                  <h5>Video Editing</h5>
                 </div>
               </Carousel>
             </div>
